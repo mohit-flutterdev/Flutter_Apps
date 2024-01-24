606f59e3629f9e6119d063321cc8094ef126dc9b
mohit-flutterdev <mohitflutter13@gmail.com>
2024-01-24 10:02:21 +0530

    Assignment Codes

---
+++ Assignment11(24 Jan)/assignment11/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:"Assignment11",
+      home:Scaffold(
+        appBar:AppBar(
+          title:const Text("Assignmnent11"),
+          centerTitle:true,
+        ),
+
+        body:SizedBox(
+
+          child:Column(
+
+              mainAxisAlignment:MainAxisAlignment.spaceEvenly,
+              children:[
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children:[
+
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.red,
+                    ),
+
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.amber,
+                    ),
+
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.blue,
+                    ),
+                  ],
+                ),
+                Row(
+                
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children:[
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.purple,
+                    ),
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.black,
+                    ),
+                    Container(
+                      height:100,
+                      width:100,
+                      color:Colors.pink,
+                    ),
+
+                  ],
+                ),
+              ],
+          ),
+        ),
+      ),
+    );
+  }
+}
